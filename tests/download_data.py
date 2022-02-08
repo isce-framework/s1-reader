b469b98822eadd15943d69824347d3e63331e97d
vbrancat <virginia.brancato@jpl.nasa.gov>
2022-02-07 20:02:14 -0800

    Replace shutil with wget

--- tests/download_data.py
+++ tests/download_data.py
@@ -1,6 +1,4 @@
 import os
-import shutil
-from urllib.request import urlopen
 
 s3_url = "http://sentinel1-slc-seasia-pds.s3-website-ap-southeast-1.amazonaws.com/datasets/slc/v1.1"
 data_info = "2021/08/15"
@@ -8,11 +6,10 @@ granule_id = "S1A_IW_SLC__1SDV_20210815T100025_20210815T100055_039238_04A1E8_D14
 
 
 def download_granule(in_url):
-    zip_file = in_url.split('/')[-1]
-    if os.path.isfile(zip_file) == False:
+    zip_file = os.path.basename(in_url)
+    if not os.path.isfile(zip_file):
         print(f'Start downloading of {in_url}')
-        with urlopen(in_url) as response, open(zip_file, 'wb') as ofile:
-            shutil.copyfileobj(response, ofile)
+        os.system(f'wget {in_url}')
 
 
 def test_data_download():
