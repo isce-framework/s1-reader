c0ab245d454b11e9643f628d8f765bf073629d59
otirmizi <osman_tirmizi@hotmail.com>
2022-05-23 12:24:14 -0500

    added burst plotting script

---
+++ src/s1reader/utils/plot_bursts.py
@@ -0,0 +1,127 @@
+import argparse
+import glob
+from s1reader.s1_orbit import get_orbit_file_from_list
+from s1reader.s1_reader import load_bursts
+from osgeo import gdal, osr
+from shapely.geometry import Polygon
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import folium
+from shapely import wkt
+
+def command_line_parser():
+    '''
+    Command line parser
+    '''
+    parser = argparse.ArgumentParser(description="""
+                                     Create a burst map for the stack""",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-r', '--reference', type=str, action='store',
+                        dest='reference',
+                        help="Reference SAFE file for the stack")
+    parser.add_argument('-d', '--orbit-dir', type=str, dest='orbit_dir',
+                        help="Directory containing orbit files")
+    parser.add_argument('-x', '--x-spacing', type=float, default=5,
+                        dest='x_spacing',
+                        help='Spacing of the geogrid in x direction')
+    parser.add_argument('-y', '--y-spacing', type=float, default=10,
+                        dest='y_spacing',
+                        help='Spacing of the geogrid in y direction')
+    parser.add_argument('-e', '--epsg', type=int, dest='epsg',
+                        help='EPSG for output coordinates')
+    parser.add_argument('-o', '--output', type=str, default='burst_map.gpkg',
+                        dest='output',
+                        help='Output filename for burst map')
+    return parser.parse_args()
+
+
+def burst_map(reference, orbit_dir, x_spacing,
+              y_spacing, epsg,
+              output_filename):
+
+    # Initialize dictionary that will contain all the info for geocoding
+    burst_map = {'burst_id':[], 'length': [], 'width': [],
+                 'spacing_x': [], 'spacing_y':[], 'min_x': [],
+                 'max_x': [], 'min_y': [], 'max_y': [], 'first_valid_line': [],
+                 'last_valid_line':[], 'first_valid_sample':[], 'last_valid_sample':[],
+                 'border':[]}
+    i_subswath = [1, 2, 3]
+    pol = 'vv'
+    orbit_list = glob.glob(f'{orbit_dir}/*EOF')
+    orbit_path = get_orbit_file_from_list(reference, orbit_list)
+
+    for subswath in i_subswath:
+        ref_bursts = load_bursts(reference, orbit_path, subswath, pol)
+        for burst in ref_bursts:
+            burst_map['burst_id'].append(burst.burst_id)
+            burst_map['length'].append(burst.shape[0])
+            burst_map['width'].append(burst.shape[1])
+            burst_map['spacing_x'].append(x_spacing)
+            burst_map['spacing_y'].append(y_spacing)
+            burst_map['first_valid_line'].append(burst.first_valid_line)
+            burst_map['last_valid_line'].append(burst.last_valid_line)
+            burst_map['first_valid_sample'].append(burst.first_valid_sample)
+            burst_map['last_valid_sample'].append(burst.last_valid_sample)
+
+            poly = burst.border[0]
+            # Give some margin to the polygon
+            margin = 0.001
+            poly = poly.buffer(margin)
+            burst_map['border'].append(Polygon(poly.exterior.coords).wkt)
+
+            # Transform coordinates from lat/long to EPSG
+            llh = osr.SpatialReference()
+            llh.ImportFromEPSG(4326)
+            tgt = osr.SpatialReference()
+
+            tgt_x, tgt_y = [], []
+            x, y = poly.exterior.coords.xy
+            tgt.ImportFromEPSG(int(epsg))
+            trans = osr.CoordinateTransformation(llh, tgt)
+            for lx, ly in zip(x, y):
+                dummy_x, dummy_y, dummy_z = trans.TransformPoint(ly, lx, 0)
+                tgt_x.append(dummy_x)
+                tgt_y.append(dummy_y)
+            x_min = x_spacing * round(min(tgt_x) / x_spacing)
+            y_min = y_spacing * round(min(tgt_y) / y_spacing)
+            x_max = x_spacing * round(max(tgt_x) / x_spacing)
+            y_max = y_spacing * round(max(tgt_y) / y_spacing)
+
+            # Allocate coordinates inside the dictionary
+            burst_map['min_x'].append(x_min)
+            burst_map['min_y'].append(y_min)
+            burst_map['max_x'].append(x_max)
+            burst_map['max_y'].append(y_max)
+
+    # Save generated burst map as csv
+    data = pd.DataFrame.from_dict(burst_map)
+    data.to_csv(output_filename)
+
+    # Create GeoDataFrame to plot bursts on a map
+    df = data
+    df['border'] = df['border'].apply(wkt.loads)
+    gdf = gpd.GeoDataFrame(df, crs='epsg:4326')
+    gdf2 = gdf.rename(columns={'border': 'geometry'}).set_geometry('geometry')
+    
+    # Save the GeoDataFrame as a shapefile (some people may prefer the format)
+    gdf2.to_file('gdf2.shp')
+    
+    # Plot bursts on an interactive map
+    m = gdf2.explore(
+        column="burst_id", # make choropleth based on "Burst ID" column
+        tooltip="burst_id", # show "Burst ID" value in tooltip (on hover)
+        popup=True, # show all values in popup (on click)
+        tiles="CartoDB positron", # use "CartoDB positron" tiles
+        cmap="Set1", # use "Set1" matplotlib colormap
+        style_kwds=dict(color="black") # use black outline
+       )
+
+    m.save('BurstMap.html')
+    
+
+if __name__ == '__main__':
+    cmd = command_line_parser()
+    burst_map(cmd.reference, cmd.orbit_dir,
+              cmd.x_spacing, cmd.y_spacing,
+              cmd.epsg, cmd.output)
