14569b194a1582c47ca1a4e925ff72dfd0a79353
Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>
2022-06-22 15:12:24 -0700

    s1_annotation.py - rev1 after the suggestion by @gshiroma

--- src/s1reader/s1_annotation.py
+++ src/s1reader/s1_annotation.py
@@ -21,82 +21,82 @@ class annotation:
         #Data access point for the annotation .xml file
         self.xml_et=None
 
+        #TODO: Check the SAFE name and estimate the expected polarization. 
+        #See: https://sentinel.esa.int/web/sentinel/technical-guides/sentinel-1-sar/products-algorithms/level-1-product-formatting
+    
     def _get_search_phrase(self):
         str_safe=os.path.basename(self.path_annotation).replace('.zip','')
         token_safe=str_safe.split('_')
-
-        phrase_base='{PLATFORM}-{MODE}{SUBSWATH}-{TYPE}-{POL}'.format(PLATFORM=token_safe[0].lower(),
-                MODE=token_safe[1].lower(),
-                SUBSWATH=self.subswath,
-                TYPE=token_safe[2].lower(),
-                POL=self.polarization.lower())
-
-        if self.kind=='calibration':
-            phrase_xml_to_search='calibration-{PLATFORM}-{MODE}{SUBSWATH}-{TYPE}-{POL}'.format(
-                PLATFORM=token_safe[0].lower(),
-                MODE=token_safe[1].lower(),
-                SUBSWATH=self.subswath,
-                TYPE=token_safe[2].lower(),
-                POL=self.polarization.lower())
-        elif self.kind=='noise':
-            phrase_xml_to_search='noise-{PLATFORM}-{MODE}{SUBSWATH}-{TYPE}-{POL}'.format(
-                PLATFORM=token_safe[0].lower(),
-                MODE=token_safe[1].lower(),
-                SUBSWATH=self.subswath,
-                TYPE=token_safe[2].lower(),
-                POL=self.polarization.lower())
-        elif self.kind=='':
-            phrase_xml_to_search=f'/{phrase_base}'
+        if len(token_safe)<3:
+            raise ValueError(f'Invalid path_annotation: {self.path_annotation}')
         else:
-            raise ValueError(f'Cannot recognize the annotation type {self.kind}')
+            phrase_base='{PLATFORM}-{MODE}{SUBSWATH}-{TYPE}-{POL}'.format(PLATFORM=token_safe[0].lower(),
+                    MODE=token_safe[1].lower(),
+                    SUBSWATH=self.subswath,
+                    TYPE=token_safe[2].lower(),
+                    POL=self.polarization.lower())
 
-        return phrase_xml_to_search
+            if self.kind=='calibration' or self.kind=='noise':
+                phrase_xml_to_search=f'{self.kind}-{phrase_base}'
+            elif self.kind=='':
+                phrase_xml_to_search=f'/{phrase_base}'
+            else:
+                raise ValueError(f'Cannot recognize the annotation type {self.kind}')
+
+            return phrase_xml_to_search
 
 
     def _load_xml_as_et(self): #kind: ['', 'calibration', 'noise']
         if os.path.isfile(self.path_annotation):
             if self.path_annotation.endswith('.zip'):
-                zip_in=zipfile.ZipFile(self.path_annotation,'r')
-                phrase_xml_in_zip=self._get_search_phrase()
+                #zip_in=zipfile.ZipFile(self.path_annotation,'r')
+                with zipfile.ZipFile(self.path_annotation,'r') as zip_in:
+                    phrase_xml_in_zip=self._get_search_phrase()
 
-                #search for the .xml file
-                filename_xml=None
-                for fileinfo in zip_in.filelist:
-                    if (phrase_xml_in_zip in fileinfo.filename) and (fileinfo.filename.endswith('.xml')):
-                        filename_xml=fileinfo.filename
-                        break
-                
-                #load the xml if found.
-                if filename_xml is None:
-                    raise ValueError('Cannot find the annotation file.')
-                else:
-                     self.xml_et=ET.fromstring(zip_in.read(filename_xml))
-                
-                #finalize
-                self._xml_loaded=filename_xml
-                zip_in.close()
-
+                    #search for the .xml file
+                    filename_xml=None
+                    for fileinfo in zip_in.filelist:
+                        if (phrase_xml_in_zip in fileinfo.filename) and (fileinfo.filename.endswith('.xml')):
+                            filename_xml=fileinfo.filename
+                            break
+                    
+                    #load the xml if found.
+                    if filename_xml is None:
+                        raise ValueError('Cannot find the annotation file.')
+                    else:
+                        self.xml_et=ET.fromstring(zip_in.read(filename_xml))
+                    
+                    #finalize
+                    self._xml_loaded=filename_xml
+                #zip_in.close()
 
             elif self.path_annotation.endswith('.xml'):
                 #load the xml as file
                 self.xml_et=ET.parse(self.path_annotation)
                 self._xml_loaded=self.xml_et
+            else:
+                raise ValueError(f'Invalid path for the annotation: {self.path_annotation}')
 
                 
 
         elif os.path.isdir(self.path_annotation):
             phrase_xml_in_dir=self._get_search_phrase()
             if self.kind=='':
-                list_file_xml=glob.glob(f'{self.path_annotation}/{phrase_xml_in_dir}*.xml')
+                list_file_xml=glob.glob(f'{self.path_annotation}/annotation/{phrase_xml_in_dir}*.xml')
+            elif self.kind=='calibration' or self.kind=='noise':
+                list_file_xml=glob.glob(f'{self.path_annotation}/annotation/calibration/{phrase_xml_in_dir}*.xml')
             else:
-                list_file_xml=glob.glob(f'{self.path_annotation}/annotation/{self.kind}-{phrase_xml_in_dir}*.xml')
-            
+                raise ValueError(f'Annotation kind was not provided or not recognized: {self.kind}')
+
             if len(list_file_xml)==1:
                 self.xml_et=ET.parse(list_file_xml[0])
                 self._xml_loaded=list_file_xml[0]
                 
+            elif len(list_file_xml)==0:
+                raise ValueError(f'Cannot find the corresponding .xml file')
             else:
-                raise ValueError(f'Search for the annotation file does not look right. len={len(list_file_xml)}')
+                #raise ValueError(f'Search for the annotation file does not look right. len={len(list_file_xml)}')
+                raise ValueError('More than one .xml files are found:\n'+'\n'.join(list_file_xml))
 
 
     def _parse_VectorList(self,name_VectorList:str,name_vector:str,str_type:str):
@@ -151,10 +151,11 @@ class annotation:
                 if self.path_annotation.endswith('.zip'):
                     zip_in=zipfile.ZipFile(self.path_annotation,'r')
                     str_manifest=zip_in.read(f'{str_safe}.SAFE/manifest.safe').decode()
-                    
             elif os.path.isdir(self.path_annotation):
                 with open(f'{self.path_annotation}/manifest.safe','r') as manifest_in:
                     str_manifest=manifest_in.read()
+            else:
+                raise ValueError(f'Cannot find the manifest.safe file using path_annotation: {self.path_annotation}')
 
             lines_manifest=str_manifest.split('\n')
             for line_manifest in lines_manifest:
@@ -231,6 +232,10 @@ class calibration(annotation):
 
 
 class noise(annotation):
+    #TODO: Schema of the NADS is slightly different before/after IPF version 2.90. Needs to be adaptive in accordance with the version.
+    #in ISCE2 code: if float(self.IPFversion) < 2.90:
+    # REF: .../isce2/components/isceobj/Sensor/GRD/Sentinel1.py
+
     def __init__(self,path_annotation,polarization='VH',subswath=1):
         super().__init__(path_annotation,polarization,subswath)
         self.kind='noise'
