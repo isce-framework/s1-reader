e642bd645aea820e03a3497df9de1c647cb71f0a
Ryan Burns <rtburns@jpl.nasa.gov>
2022-06-22 23:40:13 -0700

    Add basic circleci config (#36)
    * Add basic circleci config
    
    * Add specfile and auto-specfile-updater script to .circleci directory
    
    * Update circleci config to use conda with specfile for all deps
    
    * Add sh shebang
    
    * Quote sh variable expansion

---
+++ .circleci/freeze-deps.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker run -v "$PWD:/mnt" -w /mnt --rm -it continuumio/miniconda3 bash -c 'conda install -y -c conda-forge isce3=0.6 build setuptools && conda list --explicit > specfile.txt'
