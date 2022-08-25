358a431b51ff66854927ec200dce5e3d589a215e
Ryan Burns <rtburns@jpl.nasa.gov>
2022-08-25 12:12:11 -0700

    Use conda-forge channel only

--- .circleci/freeze-deps.sh
+++ .circleci/freeze-deps.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-docker run -v "$PWD:/mnt" -w /mnt --rm -it continuumio/miniconda3 bash -c 'conda install -y -c conda-forge isce3=0.8 build setuptools pytest isce3 shapely && conda list --explicit > specfile.txt'
+docker run -v "$PWD:/mnt" -w /mnt --rm -it continuumio/miniconda3 bash -c '\
+    conda config --add channels conda-forge && \
+    conda config --remove channels defaults && \
+    conda install -y -c conda-forge isce3=0.8 build setuptools pytest isce3 shapely && conda list --explicit > specfile.txt'
