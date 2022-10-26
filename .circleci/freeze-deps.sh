ff748035a6a7c1d01c4c46a6edcc801d1e68f122
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2022-10-26 07:40:59 -0700

    update circlci deps with docker

--- .circleci/freeze-deps.sh
+++ .circleci/freeze-deps.sh
@@ -2,5 +2,5 @@
 docker run -v "$PWD:/mnt" -w /mnt --rm -it continuumio/miniconda3 bash -c '\
     conda config --add channels conda-forge && \
     conda config --remove channels defaults && \
-    conda update conda && \
-    conda install -y -c conda-forge isce3=0.8 build setuptools pytest isce3 shapely && conda list --explicit > specfile.txt'
+    conda update -y conda && \
+    conda install -y -c conda-forge isce3=0.8 build setuptools pytest isce3 shapely lxml && conda list --explicit > specfile.txt'
