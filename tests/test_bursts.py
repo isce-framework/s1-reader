c2b86020ec1bc4b8a20ee90ba34e36aa5274fb6b
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2022-11-17 19:37:59 -0500

    revert tests folder back to current version

--- tests/test_bursts.py
+++ tests/test_bursts.py
@@ -28,7 +28,7 @@ def test_burst(bursts):
         [-2056.701472691132, 353389.9614836443, -54143009.57327797]]
 
     for i, burst in enumerate(bursts):
-        expected_burst_id = f't071_{151199 + i}_iw3'
+        expected_burst_id = f't071_{151200 + i}_iw3'
         assert burst.burst_id == expected_burst_id
         assert burst.i_burst == i
         assert burst.abs_orbit_number == 32518
