8ed919ad3dfe7d04526fc93b8fbb9551f8096fe8
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2022-11-22 12:42:58 -0500

    add method to get bounds from the preview, if available

--- src/s1reader/s1_info.py
+++ src/s1reader/s1_info.py
@@ -52,7 +52,70 @@ def _plot_bursts(safe_path: Union[Path, str], output_dir="burst_maps") -> None:
     plot_bursts.burst_map(safe_path, orbit_dir, xs, ys, epsg, output_filename)
 
 
-def _get_frame_bounds(safe_path: Union[Path, str]) -> List[float]:
+def get_frame_bounds(safe_path: Union[Path, str]) -> List[float]:
+    try:
+        return _bounds_from_preview(safe_path)
+    except Exception as e:
+        warnings.warn(f"Could not get bounds for {safe_path}: {e}")
+    return _bounds_from_bursts(safe_path)
+
+
+def _bounds_from_preview(safe_path: Union[Path, str]) -> List[float]:
+    import lxml.etree as ET
+    import tempfile
+    import zipfile
+
+    # looking for:
+    # S1A_IW_SLC__1SDV_20221005T125539_20221005T125606_045307_056AA5_CB45.SAFE/preview/map-overlay.kml
+    if _is_safe_dir(safe_path):
+        overlay_path = Path(safe_path) / "preview" / "map-overlay.kml"
+    else:
+        # The name of the unzipped .SAFE directory (with .zip stripped)
+        tmp_dir = tempfile.TemporaryDirectory()
+        with zipfile.ZipFile(safe_path, "r") as zip_ref:
+            zname = [
+                zi
+                for zi in zip_ref.infolist()
+                if "preview/map-overlay.kml" in zi.filename
+            ][0]
+            map_overlay_kml = zip_ref.extract(zname, path=tmp_dir)
+
+    # Check that they have all the necessary kmls
+    if not map_overlay_kml.exists():
+        raise ValueError(f"{map_overlay_kml} does not exist to get extent.")
+    root = ET.parse(overlay_path).getroot()
+
+    # point_str looks like:
+    # <coordinates>-102.552971,31.482372 -105.191353,31.887299...
+    point_str = list(elem.text for elem in root.iter("coordinates"))[0]
+    coords = [p.split(",") for p in point_str.split()]
+    lons, lats = zip(*[(float(lon), float(lat)) for lon, lat in coords])
+    return (min(lons), min(lats), max(lons), max(lats))
+
+
+# [1]+ wget https://datapool.asf.alaska.edu/SLC/SA/S1A_IW_SLC__1SDV_20221122T125539_20221122T125606_046007_058187_36E8.zip &	(wd: ~/dev/texas/mentone-earthquake-2022-11-16/data)
+#   1 <?xml version="1.0" encoding="UTF-8"?>
+#   2 <kml xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:gml="http://www.opengis.net/gml" xmlns:xfdu="urn:ccsds:schema:xfdu:1" xmlns:safe="http://www.esa.int/safe/sentinel-1.0" xmlns:s1="http://www.esa.int/safe/sentinel-1.0/sent    inel-1" xmlns:s1sar="http://www.esa.int/safe/sentinel-1.0/sentinel-1/sar" xmlns:s1sarl1="http://www.esa.int/safe/sentinel-1.0/sentinel-1/sar/level-1" xmlns:s1sarl2="http://www.esa.int/safe/sentinel-1.0/sentinel-1/sar/level-2" xmlns:gx="    http://www.google.com/kml/ext/2.2">
+#   3   <Document>
+#   4     <name>Sentinel-1 Map Overlay</name>
+#   5     <Folder>
+#   6       <name>Sentinel-1 Scene Overlay</name>
+#   7       <GroundOverlay>
+#   8         <name>Sentinel-1 Image Overlay</name>
+#   9         <Icon>
+#  10           <href>quick-look.png</href>
+#  11         </Icon>
+#  12         <gx:LatLonQuad>
+#  13           <coordinates>-102.622429,31.201340 -105.269714,31.609007 -104.947639,33.238331 -102.250290,32.832924</coordinates>
+#  14         </gx:LatLonQuad>
+#  15       </GroundOverlay>
+#  16     </Folder>
+#  17   </Document>
+#  18 </kml>
+# ~
+
+
+def _bounds_from_bursts(safe_path: Union[Path, str]) -> List[float]:
     """Get the bounding box of the frame from the union of all burst bounds.
 
     bounding box format is [lonmin, latmin, lonmax, latmax]
@@ -164,7 +227,7 @@ def main():
             _plot_bursts(path)
             continue
         elif args.bbox:
-            msg = f"{path}: {_get_frame_bounds(path)}"
+            msg = f"{path}: {get_frame_bounds(path)}"
             print(msg)
             continue
 
