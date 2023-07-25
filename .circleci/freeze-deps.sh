565bc6b603695d523e74db8db12465fd04f2f049
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2023-07-25 16:56:30 -0700

    specfile script needs `--network=host`. `conda env` has no install or -y

--- .circleci/freeze-deps.sh
+++ .circleci/freeze-deps.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-docker run -v "$PWD:/mnt" -w /mnt --rm -it continuumio/miniconda3 bash -c '\
+docker run --network=host -v "$PWD:/mnt" -w /mnt --rm -it continuumio/miniconda3 bash -c '\
     conda config --add channels conda-forge && \
     conda config --remove channels defaults && \
     conda update -y conda && \
-    conda install -y --file environment.yaml && conda list --explicit > specfile.txt'
+    conda env create --file environment.yaml && conda list --explicit > specfile.txt'
