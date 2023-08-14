ca1c865aaea186899d10c0af1921f6e9ac9bf437
Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>
2023-08-14 12:42:21 -0700

    add testcase that RFI annotation is expected but not exist; add docstring; test data updates

--- tests/test_load_rfi.py
+++ tests/test_load_rfi.py
@@ -34,8 +34,29 @@ def test_load_rfi():
         assert burst_rfi_info_dict == expected_rfi_info_list[i_burst]
 
 
+def test_skip_rfi_load():
+    '''
+    Check for the test dataset that RFI annotation is expected (based on IPF version),
+    but not in the .zip file (e.g. data downloaded using asfsmd)
+    '''
+
+    # The test dataset below has IPF version 3.52:
+    # RFI information IS expected with that version.
+    zipname = "S1A_IW_SLC__1SDV_20221016T015043_20221016T015111_045461_056FC0_6681.zip"
+    zip_path = data_path / zipname
+
+    bursts = load_bursts(zip_path, None, 2, pol="vv", flag_apply_eap=False)
+    for b in bursts:
+        assert b.burst_rfi_info is None
+
+
 def test_load_missing_rfi():
-    # Check it's skipped for zip files without RFI
+    '''
+    Check it's skipped for zip files without RFI
+    The test dataset below has IPF version 3.20:
+    RFI information is not expected with that version.
+    '''
+
     zipname = "S1A_IW_SLC__1SDV_20200511T135117_20200511T135144_032518_03C421_7768.zip"
     zip_path = data_path / zipname
     for b in load_bursts(zip_path, None, 2, pol="vv", flag_apply_eap=False):
