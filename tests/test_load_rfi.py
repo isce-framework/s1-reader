1a5bf545a05298647585e232ed0ece9dd04b1ecc
Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>
2023-08-14 12:25:22 -0700

    docstring for the test; using JSON format for the expected RFI info

--- tests/test_load_rfi.py
+++ tests/test_load_rfi.py
@@ -1,5 +1,6 @@
+import json
 from pathlib import Path
-import pickle
+
 
 from s1reader import load_bursts
 
@@ -7,22 +8,30 @@ data_path = Path(__file__).parent.resolve() / "data"
 
 
 def test_load_rfi():
+    '''
+    Check if the RFI information was loaded properly, and
+    the information in the object is as expected.
+    '''
     zipname = "S1A_IW_SLC__1SDV_20230108T135249_20230108T135316_046693_0598D3_BA76.zip"
-    rfi_info_pickle_filename = "rfi_info_list.pickle"
+    rfi_info_pickle_filename = "rfi_info_list.json"
     zip_path = data_path / zipname
     rfi_info_path = data_path / rfi_info_pickle_filename
 
     # load the expected RFI info
-    with open(rfi_info_path, 'rb') as file_in:
-        expected_rfi_info_list = pickle.load(file_in)
+    with open(rfi_info_path, 'r') as file_in:
+        expected_rfi_info_list = json.load(file_in)
 
     bursts = load_bursts(zip_path, None, 2, pol="vv", flag_apply_eap=False)
     for i_burst, b in enumerate(bursts):
         # Check if the burst RFI info has loaded
         assert b.burst_rfi_info is not None
 
+        burst_rfi_info_dict = b.burst_rfi_info.__dict__
+        burst_rfi_info_dict['rfi_burst_report']['azimuthTime'] = \
+            burst_rfi_info_dict['rfi_burst_report']['azimuthTime'].isoformat()
+
         # Check if the information in the burst RFI into is correct
-        assert b.burst_rfi_info.__dict__ == expected_rfi_info_list[i_burst]
+        assert burst_rfi_info_dict == expected_rfi_info_list[i_burst]
 
 
 def test_load_missing_rfi():
