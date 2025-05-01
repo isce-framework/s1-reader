f1c8aa305d025cff339e7cca0065844a81aebb5e
Seongsu Jeong <27862199+seongsujeong@users.noreply.github.com>
2025-05-01 15:21:22 -0700

    bump version from `0.2.4->0.2.5` (#147)
    Co-authored-by: Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>

--- src/s1reader/version.py
+++ src/s1reader/version.py
@@ -4,6 +4,7 @@ import collections
 # release history
 Tag = collections.namedtuple("Tag", "version date")
 release_history = (
+    Tag("0.2.5", "2025-05-01"),
     Tag("0.2.4", "2024-03-11"),
     Tag("0.2.3", "2023-09-21"),
     Tag("0.2.2", "2023-09-08"),
